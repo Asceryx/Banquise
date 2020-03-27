0110aeeb83b71dc103a16255ee4caa52141809b1
Asceryx <jhayrault@gmail.com>
2020-03-27 11:38:19 +0100

    correctif_3

--- display.c
+++ display.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "object.h"
+#include "player.h"
 #include "packice.h"
 
 
@@ -10,21 +12,7 @@
 void show(PackIce packice)
 {
 	int max_size = packice.size;
-	for (int i = 0; i<max_size; i++)
-	{
-		for (int j = 0; j < max_size; j++)
-		{
-			Ice block = getBlock(packice,i,j);
-			if (!(block.is_water)) {
-				printf(" |%d : S : " , block.pos->x);
-			}
-			else {
-				printf(" |%d : L : " , block.pos->x);
-			}
-			printf("%d| " , block.pos->y);
-		}
-		printf("\n");
-	}
+	printf("%d\n", max_size);
 }
 
 
